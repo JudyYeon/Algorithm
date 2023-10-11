4b260e6340db6781c236468cc2f0deaf0659e4be
JudyYeon <dev.judykim@gmail.com>
2023-10-11 23:44:28 +0900

    Comment: java language로 재풀이

---
+++ Week01/Sort/PS2750_SortNum.java
@@ -0,0 +1,13 @@
+package Sort;
+
+import java.util.Scanner;
+
+public class PS2750_SortNum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+        int answer = 0;
+
+
+    }
+}
---
+++ Week01/Stack/PS9012_ParenthesisString.java
@@ -0,0 +1,7 @@
+package Stack;
+
+public class PS9012_ParenthesisString {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
