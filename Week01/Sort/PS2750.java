acc2e9aaafb45a10a0fd43f45e8d0792db7c220d
JudyYeon <dev.judykim@gmail.com>
2023-10-13 01:58:15 +0900

    Feat: Sort
    Arrays 패키지 활용

--- Week01/Sort/PS2750.java
+++ Week01/Sort/PS2750.java
@@ -1,13 +1,25 @@
 package Sort;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PS2750 {
+    /*
+     * title : 수 정렬하기
+     * url : https://www.acmicpc.net/problem/2750
+     * */
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int testCase = sc.nextInt();
-        int answer = 0;
 
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        for(int i = 0; i < n; i++){
+            num[i] = sc.nextInt();
+        }
+        Arrays.sort(num);
+        for (int j : num) {
+            System.out.println(j);
+        }
 
     }
 }
