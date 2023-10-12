ddebdfbd06c3386e4daacc12ed440fd60a75ed3c
JudyYeon <dev.judykim@gmail.com>
2023-10-13 00:07:13 +0900

    Feat: Array
    Week02 Array 관련 문제풀이 및 회고록 작성

---
+++ Week02/Array/PS11720.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Scanner;
+
+public class PS11720 {
+    /*
+    * title : 숫자의 합
+    * url : https://www.acmicpc.net/problem/11720
+    * */
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+        int sum = 0;
+
+        for(int i = 0; i < n; i++){
+            sum += arr[i] - 48;
+        }
+        System.out.println(sum);
+
+    }
+}
--- Week02/Array/PS1546.java
+++ Week02/Array/PS1546.java
@@ -1,7 +1,34 @@
 package Array;
 
+import java.util.Scanner;
+
 public class PS1546 {
+    /*
+     * title : 평균
+     * url : https://www.acmicpc.net/problem/1546
+     * */
     public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();//입력받을 점수의 개수
+        int max = 0;    //점수 최댓값
+        double[] score = new double[n];   //점수 배열
+        double avg = 0;// avg 평균 구하기
+
+        for (int i=0; i< n; i++){
+            score[i] = sc.nextInt();
+            if(i == 0 || max <= score[i]){// max 값 변경
+                max = (int) score[i];
+            }
+        }
+        // max 값으로 점수 고치기
+        for(int i = 0; i < n; i++){
+            score[i] = score[i]/max*100;
+            avg += score[i];
+        }
+        avg /= n;
+        System.out.println(avg);
 
     }
 }
