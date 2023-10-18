3f65971754b033977ca6f84f477cd80262f235d6
JudyYeon <dev.judykim@gmail.com>
2023-10-18 23:23:36 +0900

    Feat: PS2018
    2018 문제를 수리적 접근으로 해결하였습니다.

--- src/week02/array/PS2018.java
+++ src/week02/array/PS2018.java
@@ -1,4 +1,26 @@
 package week02.array;
 
+import java.util.Scanner;
+
 public class PS2018 {
+    /*
+     * title : 수들의 합5
+     * url : https://www.acmicpc.net/problem/2018
+     * */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();   //연속된 자연수의 합으로 나타 낼 정수
+
+        int cnt = 1, result = 1; //자릿수와 결과 출력할 변수설정
+        n -= cnt;
+        while (0 < n) {
+            cnt++;
+            n -= cnt;
+            if(n % cnt == 0) result++;
+        }
+
+        System.out.println(result);
+
+    }
 }
