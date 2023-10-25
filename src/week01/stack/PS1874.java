e1eecd554876527ba813296750b8c64790fc8b04
JudyYeon <dev.judykim@gmail.com>
2023-10-25 22:57:08 +0900

    PS1874 스택수열 푸는중

---
+++ src/week01/stack/PS1874.java
@@ -0,0 +1,19 @@
+package week01.stack;
+
+import java.util.Stack;
+
+public class PS1874 {
+    /*
+     * title : 스택 수열
+     * url : https://www.acmicpc.net/problem/1874
+     *
+     *  [ 효율성 ]
+     *  - 메모리:  KB
+     *  - 시간 :   ms
+     * */
+    public static void main(String[] args){
+
+        Stack<Character> stack = new Stack<>();
+        System.out.println(stack.empty());
+    }
+}
