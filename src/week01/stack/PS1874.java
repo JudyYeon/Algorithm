72768254548dda82edc56bcd6d8fdfb4eb846bd0
JudyYeon <dev.judykim@gmail.com>
2023-10-26 23:15:56 +0900

    PS1874 스택 수열 : 주석추가

--- src/week01/stack/PS1874.java
+++ src/week01/stack/PS1874.java
@@ -22,28 +22,29 @@ public class PS1874 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] A = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        int sequence = 1;
+        int[] A = new int[n];   //입력받을 배열
+        Stack<Integer> stack = new Stack<>();  
+        int sequence = 1;   //스택에 넣을 오름차순 자연수
         boolean statue = true;
-        List<Character> result = new ArrayList<>();
+        List<Character> result = new ArrayList<>(); //결과담을 곳
 
         for (int i = 0; i < n; i++) {
             A[i] = Integer.parseInt(br.readLine());
         }
 
+        // for-each로 만들어야 할 수열 A리스트를 참조하며 스택연산을 한다.
         for (int su : A) {
+
             if (su >= sequence) {
-                // 넣어야 함
+                // 입력한 수의 크기가 더 크거나 같을 때, 넣어야 함
                 while (su >= sequence) {
                     stack.push(sequence++);
                     result.add('+');
                 }
-                stack.pop();
+                stack.pop(); //나 자신 빼기
                 result.add('-');
 
             } else {
-
                 int k = stack.pop();
                 if (k > su) {
                     System.out.println("NO");
