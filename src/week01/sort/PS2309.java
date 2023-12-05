24495742bd136d78651759dd19c309749d0cc524
Judy <97666865+JudyYeon@users.noreply.github.com>
2023-12-05 10:53:45 +0900

    Create PS2309.java
    2309번 문제 스켈레톤 코드작성

---
+++ src/week01/sort/PS2309.java
@@ -0,0 +1,13 @@
+package week01.sort;
+
+
+public class PS2309 {
+    /*
+     * title : 일곱 난쟁이
+     * url : https://www.acmicpc.net/problem/2309
+     * */
+    public static void main(String[] args){
+        
+
+    }
+}
