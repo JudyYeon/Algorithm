b04653845d97d14602b6a63485eae3b42c6117c4
Judy <97666865+JudyYeon@users.noreply.github.com>
2025-06-20 22:02:36 +0900

    Update 대소문자 바꿔서 출력하기.java

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -5,6 +5,9 @@ public class Solution {
         
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
+
+        // String은 불변 객체이므로 매번 문자를 추가할 때마다 새로운 String 객체를 생성 -> 메모리가 많이 소모되고 연산 시간이 길어짐
+        // StringBuilder는 가변 객체이므로 수정하더라도 새로운 객체를 만들어내지 않기 때문에 사용함
         StringBuilder result = new StringBuilder();
         
         // 입력받은 문자열은 Array로 변환하여 하나씩 탐색
